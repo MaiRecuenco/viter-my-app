68173667626250eda75f14fe3724e1929c63fda3
aaairam <magildaceria@gmail.com>
2025-07-17 13:59:30 +0800

    commit

--- src/components/pages/home/js/toggleMenu.js
+++
@@ -1,7 +0,0 @@
-const headerNav = document.querySelector(".headerNav ul");
-const toggleMenu = document.querySelector(".toggleMenu");
-
-toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  headerNav.classList.toggle("open");
-});
